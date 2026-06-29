339fe6e05e619cd45d8cbae994faa345efc3ec74
alsventurini-ai <alsventurini@gmail.com>
2026-06-29 20:24:07 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'gc-tarefas-v35';
+const urlsToCache = [
+  '/G-C-Tarefas/tarefas-diarias.html',
+  '/G-C-Tarefas/manifest.json',
+  '/G-C-Tarefas/icon-192.png',
+  '/G-C-Tarefas/icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(key) { return key !== CACHE_NAME; })
+            .map(function(key) { return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
